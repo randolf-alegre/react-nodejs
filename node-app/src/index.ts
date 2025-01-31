eca625c9306c0c5939aac169ef15da2e59d79f6d
Randolf Alegre <dolf.alegre1989@gmail.com>
2025-02-01 02:57:25 +0800

    reset db

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors'
 import express from 'express';
 import { routes } from './routes';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,7 +12,7 @@ app.use(cors())
 app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-dotenv.config();
+
 
 app.use('/', routes);
 
